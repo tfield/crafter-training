1960a9e3a0558dbe09ccff874ade0de054c72d71
Tony Field <tony@kingsway.digital>
2021-01-27 14:43:36 -0500

    User tony wrote content /scripts/graphql/init.groovy

--- scripts/graphql/init.groovy
+++ scripts/graphql/init.groovy
@@ -1,4 +1,24 @@
+package graphql
+
+import static graphql.Scalars.GraphQLString
+import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition
+import static graphql.schema.GraphQLObjectType.newObject
+import static graphql.schema.GraphQLNonNull.nonNull
+import static graphql.schema.GraphQLList.list
+
+
 def OUR_FOO_DATA = [
     [title: "ABC", count: 46],
     [title: "XYZ", count: 2]
 ]
+
+def fooType = newObject()
+    .name('FooType')
+    .description('Foo Type')
+    .fields([
+        newFieldDefinition()
+        .name('title')
+        .description("Title")
+        .type(nonNull(grphQLString))
+        .build()
+        ]).build()
