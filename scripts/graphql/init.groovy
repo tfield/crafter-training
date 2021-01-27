1eba53b34d60411d39a84b2350e373ebe9d9ed65
Terry D'Entremont <tdentremont@cpac.ca>
2021-01-27 19:44:46 +0000

    User tdentremont wrote content /scripts/graphql/init.groovy

--- scripts/graphql/init.groovy
+++ scripts/graphql/init.groovy
@@ -1,4 +1,25 @@
+package graphql
+import static graphql.Scalars.GraphQLString
+import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition
+import static graphql.schema.GraphQLObjectType.newObject
+import static graphql.schema.GraphQLNonNull.nonNull
+import static graphql.schema.GraphQLList.list
+
 def OUR_FOO_DATA = [
 [title: "ABC", count: 46],
 [title: "XYZ", count: 14]
 ]
+
+// define the custom Type
+def fooType = newObject()
+.name('FooType')
+.description('Foo Type')
+.fields([
+    newFieldDefinition()
+    .name('title')
+    .description('The title of the foo')
+    .type(nonNull(GraphQLString))
+    .build()
+    ]).build()
+
+
