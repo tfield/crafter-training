e19f6e9f8b2fa8ac8afd0426f3ccf0eae004a1ea
Tony Field <tony@kingsway.digital>
2021-01-27 12:44:32 -0500

    User tony wrote content /scripts/filters/timer.groovy

--- scripts/filters/timer.groovy
+++ scripts/filters/timer.groovy
@@ -3,4 +3,4 @@ long start = System.currentTimeMillis()
 filterChain.doFilter(request, response)
 
 long elapsedMillis = System.currentTimeMillis() - start
-response.setHeader("X-TONY-ELAPSED-TIME", Long.toString(elapsedMillis))
+logger.info("Elapsed time: "+elapsedMillis)
