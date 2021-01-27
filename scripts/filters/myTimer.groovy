f8d2463f05919d733000213b2fefe983ca11a5ad
Terry D'Entremont <tdentremont@cpac.ca>
2021-01-27 17:51:53 +0000

    User tdentremont wrote content /scripts/filters/myTimer.groovy

--- scripts/filters/myTimer.groovy
+++ scripts/filters/myTimer.groovy
@@ -4,4 +4,4 @@ long start = System.currentTimeMillis()
 filterChain.doFilter(request, response)
 
 long elapsedMillis = System.currentTimeMillis() - start
-logger.info("Elapsed time: "+elapsedMillis)
+logger.info("Elapsed time for URI: "+request.getRequestURI() + ": "+elapsedMillis)
